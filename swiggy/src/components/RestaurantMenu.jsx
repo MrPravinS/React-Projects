110f6d14cd4a5233b34cfb2d32ce0ae5990351f9
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-01-17 08:45:35 +0530

    menu_Items

--- swiggy/src/components/RestaurantMenu.jsx
+++ swiggy/src/components/RestaurantMenu.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
 import Loading from "./Loading"
+import {Link, useParams} from "react-router-dom"
 
 const RestaurantMenu= () =>{
     const [resInfo,setResInfo] = useState(null)
+
+    const {restId} = useParams()
 
     useEffect(()=>{
         fetchMenu()
@@ -20,8 +23,9 @@ const RestaurantMenu= () =>{
   
      if(resInfo == null) return <Loading/>;
 
-    // const{itemCard} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-    // console.log(itemCard);
+     const itemCard = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.carousel[0]?.dish?.info?.name;
+     console.log(itemCard);
+     
     
  return (
     <div>
@@ -30,10 +34,14 @@ const RestaurantMenu= () =>{
         } - {resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}</p>
         <h2>Menu</h2>
         <ul>
-            <li>{resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.name}</li>
+            <Link><li>{resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.name} - {resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.price/100 || resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.defaultPrice}</li></Link>
             <li>{resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[1].dish.info.name}</li>
-            {/* <li>{itemCard?.carousel[0]?.dish?.info.name}</li> */}
-            <li>Diet Coke</li>
+            {/* <li>{itemCard?.carousel[0]?.dish?.info.name}</li>
+            {/* <li>Diet Coke</li>  */}
+           {/* {itemCard.map((item)=>{
+            <li>{item.name}</li>
+           })} */}
+          
         </ul>
     </div>
       
---
+++ swiggy/src/utils/constant.js
@@ -0,0 +1,3 @@
+
+
+export const MENU_API  = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId="
