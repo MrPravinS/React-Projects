691c6efe6a1918474656573baea63fe9d5ed2274
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-01-24 23:40:10 +0530

    routiing

--- swiggy/src/App.jsx
+++ swiggy/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from "./components/Footer"
-import './App.css'
 import { Outlet } from 'react-router-dom'
 
 function App() {
--- swiggy/src/components/Error.jsx
+++ swiggy/src/components/Error.jsx
@@ -1,14 +1,14 @@
 import { useRouteError } from "react-router-dom"
 
 const Error = () => {
-    const err = useRouteError()
+    const err = useRouteError()  // 
     console.log(err);
     
  return (
     <div>
-        <h1>Oops!</h1>
+        <h1 className="">Oops!</h1>
         <p>Somthing gone wrong</p>
-        <h3>{err.status}:{err.statusText}</h3>
+        <h3>{err.status}: {err.statusText}</h3>
     </div>
  )
 
