b6e250a1513f84b204b4a7b9d047e1194cc85b0c
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-01-25 23:58:03 +0530

    footer

--- swiggy/src/components/Footer.jsx
+++ swiggy/src/components/Footer.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-300'>
-     <h1>Footer</h1>
+       <div className="flex justify-around p-2">
+       <div className='flex ml-4'>
+        <h1 className='text-2xl font-medium'>For better experience,download the Swiggy app now</h1>
+        </div>
+       <div className='h-[200px] w-[300px]'>
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png" alt="google-store-logo" />
+       </div>
+       <div className='h-[200px] w-[300px]'>
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/app_store.png" alt="apple-store" />
+       </div>
+       </div>
     </div>
   )
 }
