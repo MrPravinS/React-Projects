a37b7c020f8431e9c5dbf1f627759ce2ade1d29c
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-01-30 23:26:44 +0530

    routing

--- swiggy/src/components/Footer.jsx
+++ swiggy/src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
        <div className='flex ml-4'>
         <h1 className='text-2xl font-medium'>For better experience,download the Swiggy app now</h1>
         </div>
-       <div className='h-[200px] w-[300px]'>
+       <div className='h-[200px] w-[300px] ml-5'>
         <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png" alt="google-store-logo" />
        </div>
-       <div className='h-[200px] w-[300px]'>
+       <div className='h-[200px] w-[300px] mr-8'>
         <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/app_store.png" alt="apple-store" />
        </div>
        </div>
--- swiggy/src/components/RestaurantMenu.jsx
+++ swiggy/src/components/RestaurantMenu.jsx
@@ -1,52 +1,70 @@
-import { useEffect, useState } from "react"
-import Loading from "./Loading"
-import {Link, useParams} from "react-router-dom"
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import { Link, useParams } from "react-router-dom";
 
-const RestaurantMenu= () =>{
-    const [resInfo,setResInfo] = useState(null)
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
 
-    const {restId} = useParams()
+  const { restId } = useParams();
 
-    useEffect(()=>{
-        fetchMenu()
-    },[])
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-    const fetchMenu =  async() => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId=56370&catalog_qa=undefined&submitAction=ENTER")
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId=56370&catalog_qa=undefined&submitAction=ENTER"
+    );
 
-        const json = await data.json()
-        console.log(json);
+    const json = await data.json();
+    console.log(json);
 
-        setResInfo(json.data)
-        
-    }
-  
-     if(resInfo == null) return <Loading/>;
+    setResInfo(json.data);
+  };
 
-     const itemCard = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.carousel[0]?.dish?.info?.name;
-     console.log(itemCard);
-     
-    
- return (
+  if (resInfo == null) return <Loading />;
+
+  const itemCard =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
+      ?.carousel[0]?.dish?.info?.name;
+  console.log(itemCard);
+
+  return (
     <div>
-        <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
-        <p>{resInfo?.cards[2]?.card?.card?.info?.cuisines.join(", ")
-        } - {resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}</p>
-        <h2>Menu</h2>
-        <ul>
-            <Link><li>{resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.name} - {resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.price/100 || resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[0].dish.info.defaultPrice}</li></Link>
-            <li>{resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card.carousel[1].dish.info.name}</li>
-            {/* <li>{itemCard?.carousel[0]?.dish?.info.name}</li>
+      <h1>{resInfo?.cards[2]?.card?.card?.info?.name}</h1>
+      <p>
+        {resInfo?.cards[2]?.card?.card?.info?.cuisines.join(", ")} -{" "}
+        {resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}
+      </p>
+      <h2>Menu</h2>
+      <ul>
+        <Link>
+          <li>
+            {
+              resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]
+                .card.card.carousel[0].dish.info.name
+            }{" "}
+            -{" "}
+            {resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]
+              .card.card.carousel[0].dish.info.price / 100 ||
+              resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]
+                .card.card.carousel[0].dish.info.defaultPrice}
+          </li>
+        </Link>
+        <li>
+          {
+            resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card
+              .card.carousel[1].dish.info.name
+          }
+        </li>
+        {/* <li>{itemCard?.carousel[0]?.dish?.info.name}</li>
             {/* <li>Diet Coke</li>  */}
-           {/* {itemCard.map((item)=>{
+        {/* {itemCard.map((item)=>{
             <li>{item.name}</li>
            })} */}
-          
-        </ul>
+      </ul>
     </div>
-      
- )
-}
+  );
+};
 
-
-export default RestaurantMenu
+export default RestaurantMenu;
