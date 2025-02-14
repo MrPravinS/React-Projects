8d41de40b206abcba8babc9e4c6e749436bd7ace
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-14 22:44:48 +0530

    reactLazy()

--- swiggy/src/main.jsx
+++ swiggy/src/main.jsx
@@ -1,53 +1,54 @@
-import { lazy, StrictMode,Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import About from './components/About'
-import Contact from './components/Contact.jsx'
-import Error from './components/Error.jsx'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import Body from './components/Body.jsx'
-import RestaurantMenu from './components/RestaurantMenu.jsx'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import About from "./components/About";
+import Contact from "./components/Contact.jsx";
+import Error from "./components/Error.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from "./components/Body.jsx";
+import RestaurantMenu from "./components/RestaurantMenu.jsx";
 // import Grocery from './components/Grodcery.jsx'
 
-
-const Grocery = lazy(()=>import("./components/Grodcery.jsx"))
+const Grocery = lazy(() => import("./components/Grodcery.jsx"));
 const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:"/",
-        element:<Body/>
+        path: "/",
+        element: <Body />,
       },
-   
+
       {
-        path:"/about",
-        element:<About/>
-      },
-      {
-        path:"/contact",
-        element:<Contact/>
+        path: "/about",
+        element: <About />,
       },
       {
-        path:"/grocery",
-        element:<Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense>
+        path: "/contact",
+        element: <Contact />,
       },
-      
       {
-        path:"/restaurant/:restId",
-        element:<RestaurantMenu/>
-      }
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+
+      {
+        path: "/restaurant/:restId",
+        element: <RestaurantMenu />,
+      },
     ],
-    errorElement:<Error/>
+    errorElement: <Error />,
   },
-  
-])
+]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={appRouter}/>
-  </StrictMode>,
-)
+    <RouterProvider router={appRouter} />
+  </StrictMode>
+);
