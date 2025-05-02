2dba6dd3352079a6906663e62b1ccaf8bd29820a
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-03 01:51:25 +0530

    build todoApp

---
+++ ToDoApp/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTask] = useState([]);
+  const [completed,setcompleted] = useState(false)
+
+  const handleTask = () => {
+    if (input.trim() !== "") {
+      setTask([...tasks, input.trim()]);
+      setInput("");
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const deleted = [...tasks];
+    deleted.splice(index, 1);
+    setTask(deleted);
+  };
+
+  const handleEnterKey = (e) => {
+    if(e.key === "Enter"){
+      handleTask()
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen  bg-gray-100">
+      <div className="w-[500px] h-[500px] shadow-2xl rounded-2xl bg-gray-200 border border-black p-6 ">
+        <h1 className="text-2xl font-semibold text-center m-4">Todo App</h1>
+
+        <input
+          type="text"
+          className="p-2 m-2  text-xl bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+          value={input}
+          onKeyDown={handleEnterKey}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 px-3 rounded-lg  py-2 text-white cursor-pointer hover:bg-blue-600"
+          onClick={() => handleTask()}
+        >
+          Add
+        </button>
+
+        {tasks.map((task, index) => (
+          <span className="flex  justify-between items-center bg-green-300 m-3 p-3 rounded-lg  " key={index}>
+            {task}{" "}
+            <button
+              onClick={() => handleDeleteTask(index)}
+              className="ml-2 text-blue-500 hover:text-red-600 cursor-pointer"
+            >
+              X
+            </button>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
