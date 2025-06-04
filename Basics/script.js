12084fb0a7b218acaeebbe6a82e9cd0b144423ab
MrPravinS <shegamwarpravin07@gmail.com>
2025-06-04 09:41:29 +0530

    Learn react Basics

---
+++ Basics/script.js
@@ -0,0 +1,15 @@
+const heading = React.createElement(
+  "div",
+  { id: "heading", class: "title" },
+  [React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hello world from the react!"),
+    React.createElement("h2", {}, "this is h2 tag!"),
+  ]),React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hello world from the react!"),
+    React.createElement("h2", {}, "this is h2 tag!"),
+  ])]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+console.log(heading);
