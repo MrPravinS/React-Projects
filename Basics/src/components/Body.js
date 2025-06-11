5a89e86f828dfd447494d3c3aa4ccc102bab5011
MrPravinS <shegamwarpravin07@gmail.com>
2025-06-11 23:42:32 +0530

    added search button

--- Basics/src/components/Body.js
+++ Basics/src/components/Body.js
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import RestroCards from "./RestroCards";
 import Loading from "./Loading"
 const Body = () => {
+  const [allFoodItem, setAllFoodItem] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
+  const [searchInput, setSearchInput] = useState("")
+  
 
   const imgId =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
@@ -20,7 +23,7 @@ const Body = () => {
     const restaurants =
       restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
       [];
-
+    setAllFoodItem(restaurants)
     setFoodItem(restaurants);
     console.log(restaurants);
   };
@@ -30,6 +33,18 @@ const Body = () => {
   }, []);
 
   return foodItem.length === 0 ? <Loading/> : (
+    <div className="">
+      <div className=" flex justify-center items-center m-3 ">
+        <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} placeholder="enter food items" type="text" className="bg-gray-200 p-2 w-[300px] rounded-full"/>
+        <button onClick={()=>{
+          const filters = allFoodItem.filter((item)=>
+            item.info.name.toLowerCase().includes(searchInput.toLowerCase())
+          )
+          
+          setFoodItem(filters);
+          setSearchInput("")
+        }} className="bg-blue-500 text-white px-2 py-2 rounded-full focus:ring-1 hover:bg-blue-600 transition duration-400 cursor-pointer font-bold">Search</button>
+      </div>
     <div className="bg-white flex flex-wrap">
       {foodItem.map((cards) => {
         return (
@@ -43,6 +58,7 @@ const Body = () => {
           />
         );
       })}
+    </div>
     </div>
   );
 };
--- Basics/src/components/RestroCards.js
+++ Basics/src/components/RestroCards.js
@@ -2,10 +2,10 @@ const RestroCards = ({area, avgRating, cloudinaryImageId, cuisines, name }) => {
   return (
     <div className="bg-white w-[250px] p-2 m-4 rounded-lg shadow-2xl mb-4 hover:scale-105 cursor-pointer transition duration-300">
       <img className="rounded-lg h-[150px] w-full" src={cloudinaryImageId} alt="" />
-      <p className="font-bold text-center">{name}</p>
-      <p>{avgRating}</p>
+      <p className="font-bold text-center mt-2">{name}</p>
+      <p className="text-xl font-bold ml-2">{avgRating}/5 🌟</p>
       <p className="text-gray-600 text-center flex flex-wrap justify-center m-2">{cuisines}</p>
-      <p>{area}</p>
+      <p className="font-semibold text-center m-2">{area}</p>
     </div>
   );
 };
